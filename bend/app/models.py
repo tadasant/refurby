1a05a3456843f253eb56792e1378532c26f3dcde
Dennis Sell <sell.dennis@gmail.com>
2019-11-23 05:35:36 -0800

    Add friendship connection and query for 2nd degree connections

--- bend/app/models.py
+++ bend/app/models.py
@@ -1,4 +1,5 @@
 import enum
+import itertools
 
 from app import db
 
@@ -8,6 +9,14 @@ class Degree(enum.Enum):
     BACHELOR = 'bachelor'
     MASTER = 'master'
     PHD = 'phd'
+
+
+friendship = db.Table(
+    'friendships',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), index=True),
+    db.Column('friend_id', db.Integer, db.ForeignKey('user.id')),
+    db.UniqueConstraint('user_id', 'friend_id', name='unique_friendships'))
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -23,8 +32,35 @@ class User(db.Model):
     linkedin_url = db.Column(db.String(64))
     image_url = db.Column(db.String(64))
 
-    def friends_of_friends(self):
-        return User.query.all()
+    friends = db.relationship(
+        'User',
+        secondary=friendship,
+        primaryjoin=id==friendship.c.user_id,
+        secondaryjoin=id==friendship.c.friend_id)
+
+
+    def befriend(self, friend):
+        if friend not in self.friends:
+            self.friends.append(friend)
+            friend.friends.append(self)
+
+    def unfriend(self, friend):
+        if friend in self.friends:
+            self.friends.remove(friend)
+            friend.friends.remove(self)
+
+    def all_second_degree_connections(self):
+        friends = self.friends
+        fofs = [f.friends for f in friends]
+        all_connections = friends + list(itertools.chain(*fofs))
+
+        connections = []
+        user_ids = set()
+        for user in all_connections:
+            if user.id != self.id and user.id not in user_ids:
+                connections.append(user)
+                user_ids.add(user.id)
+        return connections
 
     def __repr__(self):
         return '<User {}>'.format(self.email)
