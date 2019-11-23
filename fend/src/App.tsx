ef9885a73636c6bdbb1c3087d6557a9995cdc4cd
Tadas Antanavicius <3900899+tadasant@users.noreply.github.com>
2019-11-23 02:54:54 -0800

    form built

--- fend/src/App.tsx
+++ fend/src/App.tsx
@@ -1,21 +1,22 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Opportunity from "./components/opportunity/OpportunityView";
+import "./App.scss";
 
 const App: React.FC = () => {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+		<div>
+			<header></header>
+			<Router>
+				<Switch>
+					<Route path="/welcome">{/* <Welcome/> */}</Route>
+					<Route path="/dashboard">{/* <Dashboard/> */}</Route>
+					<Route path="/contacts">{/* <Contacts /> */}</Route>
+					<Route path="/opportunity">
+						<Opportunity />
+					</Route>
+				</Switch>
+			</Router>
 		</div>
 	);
 };
---
+++ fend/src/components/opportunity/create/CreateOpportunity.tsx
@@ -0,0 +1,99 @@
+import React from "react";
+import { OpportunityStep } from "../OpportunityView";
+import { FormGroup, TextArea, NumericInput } from "@blueprintjs/core";
+import { Opportunity } from "../../../types";
+import { INDUSTRY, DEGREES, US_STATE } from "../../../constants";
+import _ from "lodash";
+
+interface Props {
+	opportunity: Opportunity;
+	setOpportunity: (opportunity: Opportunity) => void;
+	setStep: (step: OpportunityStep) => void;
+}
+
+const CreateOpportunity: React.FC<Props> = props => {
+	const { opportunity, setOpportunity } = props;
+
+	const generateFieldChange = (
+		field: keyof Opportunity
+	): React.FormEventHandler<
+		HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+	> => event => {
+		const { value } = event.currentTarget;
+		setOpportunity({
+			...opportunity,
+			[field]: value
+		});
+	};
+
+	const handleExperienceChange = (value: number) => {
+		setOpportunity({
+			...opportunity,
+			minYearsExperience: value
+		});
+	};
+
+	return (
+		<div>
+			<h2>Broadcast New Opportunity</h2>
+			<FormGroup label="Title">
+				<input
+					type="text"
+					className="bp3-input"
+					onChange={generateFieldChange("title")}
+					value={opportunity.title}
+				/>
+			</FormGroup>
+			<FormGroup label="Blurb">
+				<TextArea onChange={generateFieldChange("blurb")} />
+			</FormGroup>
+			<FormGroup label="Industry">
+				<select
+					onChange={generateFieldChange("industry")}
+					value={opportunity.industry}
+				>
+					{INDUSTRY.map(industry => (
+						<option>{industry}</option>
+					))}
+				</select>
+			</FormGroup>
+			<FormGroup label="Minimum Education">
+				<select
+					onChange={generateFieldChange("highestLevelOfEducation")}
+					value={opportunity.industry}
+				>
+					{DEGREES.map(degree => (
+						<option>{degree}</option>
+					))}
+				</select>
+			</FormGroup>
+			<FormGroup label="City">
+				<input
+					type="text"
+					className="bp3-input"
+					onChange={generateFieldChange("locationCity")}
+					value={opportunity.locationCity}
+				/>
+			</FormGroup>
+			<FormGroup label="State">
+				<select
+					onChange={generateFieldChange("locationState")}
+					value={opportunity.locationState}
+				>
+					{US_STATE.map(usState => (
+						<option>{_.upperFirst(_.toLower(usState))}</option>
+					))}
+				</select>
+			</FormGroup>
+			<FormGroup label="Minimum Years of Experience">
+				<NumericInput
+					onValueChange={handleExperienceChange}
+					value={opportunity.minYearsExperience}
+					min={0}
+				/>
+			</FormGroup>
+		</div>
+	);
+};
+
+export default CreateOpportunity;
